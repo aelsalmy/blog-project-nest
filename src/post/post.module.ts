6406e15dc73ea0ae1b0a0f25bcc780ed3f2e2640
aelsalmy <abdullah.elsalmy@outlook.com>
2026-05-06 17:43:17 +0300

    fix: dependancies

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -9,10 +9,11 @@ import { MailModule } from "src/mailing/mail.module";
 import { RabbitMQModule } from "src/config/rabbitmq.module";
 import { PostMapper } from "./post.mapper";
 import { S3Service } from "src/image-upload/s3.service";
+import { UserMapper } from "src/users/user.mapper";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post , User]) , AuthModule , MailModule, RabbitMQModule],
   controllers: [PostController],
-  providers: [PostService , PostMapper , S3Service]
+  providers: [PostService , PostMapper , S3Service , UserMapper]
 })
 export class PostModule {}
