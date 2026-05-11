9c6811ade0ae6307dd951a41fa4ab1c2623f8b67
aelsalmy <abdullah.elsalmy@outlook.com>
2026-05-11 16:12:43 +0300

    fix: adjusted feed posts

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -26,13 +26,10 @@ export class PostService {
 
   async getAllPosts(page: number , limit: number){
     const [postPage , total] = await this.postRepository.findAndCount({
-      where: {isPublished: true},
-      /*
       where: {
         isPublished: true,
         isApproved: true
-      }
-      */
+      },
       skip: (page - 1) * limit,
       take: limit,
       relations: ['user'],
